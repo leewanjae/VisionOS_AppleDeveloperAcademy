7f6ad5c3e3c5883caff2cb837c9eabd72c1f735e
leewanjae <dhkswo1@gmail.com>
2024-04-08 14:18:56 +0900

    Day1

---
+++ Day1/Day1/Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day1App.swift
+//  Day1
+//
+//  Created by LeeWanJae on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
