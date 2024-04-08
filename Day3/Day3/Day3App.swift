e2c368ed6de569da3ec12a9697108cc67220f5b6
leewanjae <dhkswo1@gmail.com>
2024-04-08 16:22:42 +0900

    Day3

---
+++ Day3/Day3/Day3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day3App.swift
+//  Day3
+//
+//  Created by LeeWanJae on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Day3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
